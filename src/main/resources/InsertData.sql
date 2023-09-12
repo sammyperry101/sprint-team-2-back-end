3742192398375c760b05e76b79a1169610f4b67b
Callum <callum.esprey@kainos.com>
2023-09-12 14:20:46 +0100

    Done insert ata for Capabilities and Families

---
+++ src/main/resources/InsertData.sql
@@ -0,0 +1,93 @@
+USE Team2_CallumE;
+
+Select * FROM Capabilities;
+
+INSERT INTO Capabilities(Name) VALUES("Engineering");
+INSERT INTO Capabilities(Name) VALUES("Platforms");
+INSERT INTO Capabilities(Name) VALUES("Data and Artificial Intelligence");
+INSERT INTO Capabilities(Name) VALUES("Cyber Security");
+INSERT INTO Capabilities(Name) VALUES("Workday");
+INSERT INTO Capabilities(Name) VALUES("Experience Design");
+INSERT INTO Capabilities(Name) VALUES("Product");
+INSERT INTO Capabilities(Name) VALUES("Delivery");
+INSERT INTO Capabilities(Name) VALUES("Operations");
+INSERT INTO Capabilities(Name) VALUES("Business Development and Marketing");
+INSERT INTO Capabilities(Name) VALUES("Organisational Strategy and Planning");
+INSERT INTO Capabilities(Name) VALUES("People");
+INSERT INTO Capabilities(Name) VALUES("Commercial and Financial Management");
+INSERT INTO Capabilities(Name) VALUES("Business Services Support");
+
+INSERT INTO Families(Name, CapabilityID) VALUES("Engineering Strategy and Planning", 1);
+INSERT INTO Families(Name, CapabilityID) VALUES("Engineering", 1);
+INSERT INTO Families(Name, CapabilityID) VALUES("Architecture", 1);
+INSERT INTO Families(Name, CapabilityID) VALUES("Testing and Quality Assurance", 1);
+INSERT INTO Families(Name, CapabilityID) VALUES("Product Specialist", 1);
+INSERT INTO Families(Name, CapabilityID) VALUES("Platform Strategy and Planning", 2);
+INSERT INTO Families(Name, CapabilityID) VALUES("Platform Engineering", 2);
+INSERT INTO Families(Name, CapabilityID) VALUES("Platform Architecture", 2);
+INSERT INTO Families(Name, CapabilityID) VALUES("Platform Specialists", 2);
+INSERT INTO Families(Name, CapabilityID) VALUES("Cloud Migration", 2);
+INSERT INTO Families(Name, CapabilityID) VALUES("Systems Support", 2);
+INSERT INTO Families(Name, CapabilityID) VALUES("Data Engineering", 3);
+INSERT INTO Families(Name, CapabilityID) VALUES("Data Architecture", 3);
+INSERT INTO Families(Name, CapabilityID) VALUES("Artificial Intelligence (AI) Engineering", 3);
+INSERT INTO Families(Name, CapabilityID) VALUES("Data Science", 3);
+INSERT INTO Families(Name, CapabilityID) VALUES("Data Consulting", 3);
+INSERT INTO Families(Name, CapabilityID) VALUES("Data Analytics", 3);
+INSERT INTO Families(Name, CapabilityID) VALUES("Security Strategy and Planning", 4);
+INSERT INTO Families(Name, CapabilityID) VALUES("Security Engineering", 4);
+INSERT INTO Families(Name, CapabilityID) VALUES("Corporate Security", 4);
+INSERT INTO Families(Name, CapabilityID) VALUES("Strategy and Planning", 5);
+INSERT INTO Families(Name, CapabilityID) VALUES("HCM", 5);
+INSERT INTO Families(Name, CapabilityID) VALUES("Financials", 5);
+INSERT INTO Families(Name, CapabilityID) VALUES("Data", 5);
+INSERT INTO Families(Name, CapabilityID) VALUES("Integrations", 5);
+INSERT INTO Families(Name, CapabilityID) VALUES("Change & User Adoption", 5);
+INSERT INTO Families(Name, CapabilityID) VALUES("Adaptive Planning", 5);
+INSERT INTO Families(Name, CapabilityID) VALUES("Product Services", 5);
+INSERT INTO Families(Name, CapabilityID) VALUES("Product Development", 5);
+INSERT INTO Families(Name, CapabilityID) VALUES("Extend", 5);
+INSERT INTO Families(Name, CapabilityID) VALUES("Spend Management", 5);
+INSERT INTO Families(Name, CapabilityID) VALUES("Insights", 6);
+INSERT INTO Families(Name, CapabilityID) VALUES("Content Design", 6);
+INSERT INTO Families(Name, CapabilityID) VALUES("UX / Visual Design", 6);
+INSERT INTO Families(Name, CapabilityID) VALUES("Service Design", 6);
+INSERT INTO Families(Name, CapabilityID) VALUES("Product Consultancy", 7);
+INSERT INTO Families(Name, CapabilityID) VALUES("Digital Advisory Consultancy", 7);
+INSERT INTO Families(Name, CapabilityID) VALUES("Low Code Consultancy", 7);
+INSERT INTO Families(Name, CapabilityID) VALUES("Delivery Strategy & Planning", 8);
+INSERT INTO Families(Name, CapabilityID) VALUES("Delivery Management", 8);
+INSERT INTO Families(Name, CapabilityID) VALUES("Engineering Management", 8);
+INSERT INTO Families(Name, CapabilityID) VALUES("Service Management", 8);
+INSERT INTO Families(Name, CapabilityID) VALUES("Workday Engagement Management", 8);
+INSERT INTO Families(Name, CapabilityID) VALUES("Operations Strategy and Planning", 9);
+INSERT INTO Families(Name, CapabilityID) VALUES("Operational Management", 9);
+INSERT INTO Families(Name, CapabilityID) VALUES("Staff Management", 9);
+INSERT INTO Families(Name, CapabilityID) VALUES("Programme Management Office (PMO)", 9);
+INSERT INTO Families(Name, CapabilityID) VALUES("Strategy and Planning", 10);
+INSERT INTO Families(Name, CapabilityID) VALUES("Business Development", 10);
+INSERT INTO Families(Name, CapabilityID) VALUES("Client Management", 10);
+INSERT INTO Families(Name, CapabilityID) VALUES("Partners", 10);
+INSERT INTO Families(Name, CapabilityID) VALUES("Bid Production", 10);
+INSERT INTO Families(Name, CapabilityID) VALUES("Inside Sales", 10);
+INSERT INTO Families(Name, CapabilityID) VALUES("Marketing-Business", 10);
+INSERT INTO Families(Name, CapabilityID) VALUES("Marketing-Martech", 10);
+INSERT INTO Families(Name, CapabilityID) VALUES("Organisational Strategy & Planning", 11);
+INSERT INTO Families(Name, CapabilityID) VALUES("People Strategy and Planning", 12);
+INSERT INTO Families(Name, CapabilityID) VALUES("People Operations / Strategic Partnering", 12);
+INSERT INTO Families(Name, CapabilityID) VALUES("Engagement, Culture and Development", 12);
+INSERT INTO Families(Name, CapabilityID) VALUES("Talent Acquisition", 12);
+INSERT INTO Families(Name, CapabilityID) VALUES("Commerical and Financial Management Strategy & Planning", 13);
+INSERT INTO Families(Name, CapabilityID) VALUES("Commercial", 13);
+INSERT INTO Families(Name, CapabilityID) VALUES("Financial", 13);
+INSERT INTO Families(Name, CapabilityID) VALUES("Payroll", 13);
+INSERT INTO Families(Name, CapabilityID) VALUES("Business Services", 14);
+INSERT INTO Families(Name, CapabilityID) VALUES("Property Management", 14);
+INSERT INTO Families(Name, CapabilityID) VALUES("Travel Management", 14);
+INSERT INTO Families(Name, CapabilityID) VALUES("Corporate Social Responsibility Management", 14);
+
+
+
+
+
+
