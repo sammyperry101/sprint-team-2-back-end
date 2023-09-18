f90354d5b6791fe77a3da34b1ac74614f582f80d
Mateen Parkar <mateenparkar4@gmail.com>
2023-09-18 16:51:54 +0100

    more linting fixes

--- src/test/java/org/kainos/ea/api/AuthServiceTest.java
+++ src/test/java/org/kainos/ea/api/AuthServiceTest.java
@@ -18,7 +18,11 @@ import java.sql.SQLException;
 
 
 import static org.hibernate.validator.internal.util.Contracts.assertNotNull;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import static org.mockito.Mockito.when;
 @ExtendWith(MockitoExtension.class)
 public class AuthServiceTest {
