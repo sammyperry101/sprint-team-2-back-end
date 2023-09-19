69dbb53cf9a9555370d7ae9d7bd003a8942a12b5
Callum <callum.esprey@kainos.com>
2023-09-19 16:24:29 +0100

    commited capabilitydaotest

---
+++ src/test/java/org/kainos/ea/db/CapabilityDaoTest.java
@@ -0,0 +1,8 @@
+package org.kainos.ea.db;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class CapabilityDaoTest {
+}
