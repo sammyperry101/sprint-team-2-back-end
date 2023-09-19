a7af4e68cbf080740ac4601eeac3fc2f4fc2f53f
sammyperry101 <sammy2580@hotmail.com>
2023-09-19 11:40:29 +0100

    Added jobFamily classes for dao, service and controller

--- src/main/java/org/kainos/ea/DropwizardWebServiceApplication.java
+++ src/main/java/org/kainos/ea/DropwizardWebServiceApplication.java
@@ -5,6 +5,10 @@ import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 import io.federecio.dropwizard.swagger.SwaggerBundle;
 import io.federecio.dropwizard.swagger.SwaggerBundleConfiguration;
+import org.kainos.ea.api.JobFamilyService;
+import org.kainos.ea.db.DatabaseConnector;
+import org.kainos.ea.db.JobFamilyDao;
+import org.kainos.ea.resources.JobFamilyController;
 
 public class DropwizardWebServiceApplication extends Application<DropwizardWebServiceConfiguration> {
 
@@ -30,7 +34,8 @@ public class DropwizardWebServiceApplication extends Application<DropwizardWebSe
     @Override
     public void run(final DropwizardWebServiceConfiguration configuration,
                     final Environment environment) {
-        // TODO: implement application
+        environment.jersey().register(new JobFamilyController(
+                new JobFamilyService(new JobFamilyDao(new DatabaseConnector()))));
     }
 
 }
---
+++ src/main/java/org/kainos/ea/resources/JobFamilyController.java
@@ -0,0 +1,41 @@
+package org.kainos.ea.resources;
+
+import io.swagger.annotations.Api;
+import org.kainos.ea.api.JobFamilyService;
+import org.kainos.ea.client.FailedToGetJobFamilyException;
+import org.kainos.ea.client.JobFamilyNotFoundException;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Api("Team 2 Role API")
+@Path("/api")
+public class JobFamilyController {
+
+    private JobFamilyService jobFamilyService;
+
+    public JobFamilyController(JobFamilyService jobFamilyService) {
+        this.jobFamilyService = jobFamilyService;
+    }
+
+    @GET
+    @Path("/view-families-by-capability/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getFamilyByCapability(@PathParam("id") int id) {
+        try {
+            System.out.println(id);
+
+            return Response.ok(jobFamilyService.getFamilyByCapability(id)).build();
+        } catch (JobFamilyNotFoundException e) {
+            System.err.println(e.getMessage());
+
+            return Response.status(Response.Status.BAD_REQUEST)
+                    .entity(e.getMessage()).build();
+        } catch (FailedToGetJobFamilyException e) {
+            System.err.println(e.getMessage());
+
+            return Response.serverError().build();
+        }
+    }
+}
