aa6fa4f30b6877a8098325854cdd445a11bdd3ee
sammyperry101 <sammy2580@hotmail.com>
2023-09-19 13:27:13 +0100

    added integration tests

---
+++ src/test/java/org/kainos/ea/integration/JobFamilyIntegrationTest.java
@@ -0,0 +1,37 @@
+package org.kainos.ea.integration;
+
+import io.dropwizard.configuration.ResourceConfigurationSourceProvider;
+import io.dropwizard.testing.junit5.DropwizardAppExtension;
+import io.dropwizard.testing.junit5.DropwizardExtensionsSupport;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.kainos.ea.DropwizardWebServiceApplication;
+import org.kainos.ea.DropwizardWebServiceConfiguration;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@ExtendWith(DropwizardExtensionsSupport.class)
+public class JobFamilyIntegrationTest {
+
+    static final DropwizardAppExtension<DropwizardWebServiceConfiguration> APP =
+            new DropwizardAppExtension<>(
+                    DropwizardWebServiceApplication.class,
+                    null,
+                    new ResourceConfigurationSourceProvider()
+            );
+
+    @Test
+    void getFamilyByCapability_shouldReturnListOfFamilies_whenValidCapabilityIdUsed() {
+        int capabilityID = 1;
+
+        List responseBody = APP.client()
+                .target("http://localhost:8080/api/view-families-by-capability/" + capabilityID)
+                .request()
+                .get(List.class);
+
+        assertFalse(responseBody.isEmpty());
+    }
+}
