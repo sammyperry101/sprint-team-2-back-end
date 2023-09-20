350b9b6718dee5ab497fc8b219a3e96b2d1433e3
Callum <callum.esprey@kainos.com>
2023-09-20 09:08:41 +0100

    removed unecessary family stuff

--- src/main/java/org/kainos/ea/client/FailedToGetFamilyException.java
+++
@@ -1,8 +0,0 @@
-package org.kainos.ea.client;
-
-public class FailedToGetFamilyException extends Throwable{
-    @Override
-    public String getMessage(){
-        return "Failed to family";
-    }
-}
--- src/main/java/org/kainos/ea/client/FamilyDoesNotExistException.java
+++
@@ -1,8 +0,0 @@
-package org.kainos.ea.client;
-
-public class FamilyDoesNotExistException extends Throwable{
-    @Override
-    public String getMessage(){
-        return "Family does not exist";
-    }
-}
