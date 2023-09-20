ccf11214c37c647d1bfe961d53e962dbf2e2a094
sammyperry101 <sammy2580@hotmail.com>
2023-09-20 14:05:04 +0100

    added bad request test for job capability integration

--- src/test/java/org/kainos/ea/integration/JobCapabilityIntegrationTest.java
+++ src/test/java/org/kainos/ea/integration/JobCapabilityIntegrationTest.java
@@ -8,10 +8,13 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.kainos.ea.DropwizardWebServiceApplication;
 import org.kainos.ea.DropwizardWebServiceConfiguration;
 
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @ExtendWith(DropwizardExtensionsSupport.class)
 public class JobCapabilityIntegrationTest {
@@ -42,5 +45,17 @@ public class JobCapabilityIntegrationTest {
                 .get(Object.class);
 
         assertNotNull(responseObject);
+    }
+
+    @Test
+    void getCapabilityById_shouldReturn400Response_whenInvalidCapabilityIdUsed() {
+        int capabilityID = -1;
+
+        Response response = APP.client()
+                .target("http://localhost:8080/api/capability/" + capabilityID)
+                .request()
+                .get();
+
+        assertEquals(400, response.getStatus());
     }
 }
