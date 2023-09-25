d610b7194a1a59c22e0574859f9a20e067037cc4
Callum <callum.esprey@kainos.com>
2023-09-25 13:12:18 +0100

    finished JobRoleFilter

--- src/main/java/org/kainos/ea/cli/JobRoleFilter.java
+++ src/main/java/org/kainos/ea/cli/JobRoleFilter.java
@@ -12,5 +12,34 @@ public class JobRoleFilter {
     @JsonCreator
     JobRoleFilter(
             @JsonProperty("roleNameFilter") String roleNameFilter,
-            @JsonProperty
+            @JsonProperty("bandNameFilter") String bandNameFilter,
+            @JsonProperty("capabilityNameFilter") String capabilityNameFilter){
+        this.roleNameFilter = roleNameFilter;
+        this.bandNameFilter = bandNameFilter;
+        this.capabilityNameFilter = capabilityNameFilter;
+    }
+
+    public String getRoleNameFilter() {
+        return roleNameFilter;
+    }
+
+    public void setRoleNameFilter(String roleNameFilter) {
+        this.roleNameFilter = roleNameFilter;
+    }
+
+    public String getBandNameFilter() {
+        return bandNameFilter;
+    }
+
+    public void setBandNameFilter(String bandNameFilter) {
+        this.bandNameFilter = bandNameFilter;
+    }
+
+    public String getCapabilityNameFilter() {
+        return capabilityNameFilter;
+    }
+
+    public void setCapabilityNameFilter(String capabilityNameFilter) {
+        this.capabilityNameFilter = capabilityNameFilter;
+    }
 }
