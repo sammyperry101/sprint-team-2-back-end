3057e081440c797bde3cb7cf84f468af308cd8f1
Adam Uddin <adamuddin@outlook.com>
2023-09-26 09:17:50 +0100

    finished testing for edit a role

--- src/test/java/org/kainos/ea/integration/JobRoleIntegrationTest.java
+++ src/test/java/org/kainos/ea/integration/JobRoleIntegrationTest.java
@@ -8,6 +8,16 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.kainos.ea.DropwizardWebServiceApplication;
 import org.kainos.ea.DropwizardWebServiceConfiguration;
+import org.kainos.ea.cli.JobRole;
+import org.kainos.ea.cli.JobRoleRequest;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.List;
 
@@ -20,12 +30,34 @@ public class JobRoleIntegrationTest {
     );
 
     @Test
-    void viewRoles_ShouldReturnListofJobRoles(){
+    void viewRoles_ShouldReturnListofJobRoles() {
 
         List responseBody = APP.client().target("http://localhost:8080/api/job-roles")
                 .request()
                 .get(List.class);
 
         Assertions.assertTrue(responseBody.size() > 0);
+    }
+
+    @Test
+    void editRole_ShouldEditJobRoleSuccessfully() {
+        Client client = ClientBuilder.newClient();
+
+        // Define the Job Role data to update
+        JobRoleRequest updatedJobRole = new JobRoleRequest("NewName", "NewSpec", "NewResponsibilities", "NewLink", 2, 2);
+
+        // Make an HTTP PUT request to edit the Job Role (assuming a specific Job Role ID)
+        Response response = client.target("http://localhost:8080/api/job-roles/50")
+                .request()
+                .put(Entity.entity(updatedJobRole, MediaType.APPLICATION_JSON_TYPE));
+
+        try {
+            // Verify the HTTP response status code (200 OK for a successful update)
+            assertEquals(Response.Status.OK.getStatusCode(), response.getStatus());
+        } finally {
+            // Close the client and release resources
+            response.close();
+            client.close();
+        }
     }
 }
