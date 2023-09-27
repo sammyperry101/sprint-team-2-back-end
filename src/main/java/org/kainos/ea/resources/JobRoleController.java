9af3cefdf433e08cef43d965f69d54e26fc4a54d
Callum <callum.esprey@kainos.com>
2023-09-27 10:42:03 +0100

    fixed linting

--- src/main/java/org/kainos/ea/resources/JobRoleController.java
+++ src/main/java/org/kainos/ea/resources/JobRoleController.java
@@ -6,7 +6,10 @@ import org.kainos.ea.cli.JobRoleFilter;
 import org.kainos.ea.client.FailedToGetJobRoles;
 import org.kainos.ea.client.JobRolesNotFoundException;
 
-import javax.ws.rs.*;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
