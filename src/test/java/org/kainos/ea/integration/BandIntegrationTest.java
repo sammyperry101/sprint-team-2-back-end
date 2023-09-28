2f07d4d315562663ec0a29ebe17f15470b430812
Remy Thompson <remy.thompson@kainos.com>
2023-09-28 10:06:14 +0100

    I think its finally over

---
+++ src/test/java/org/kainos/ea/integration/BandIntegrationTest.java
@@ -0,0 +1,37 @@
+package org.kainos.ea.integration;
+
+import io.dropwizard.configuration.ResourceConfigurationSourceProvider;
+import io.dropwizard.testing.junit5.DropwizardAppExtension;
+import io.dropwizard.testing.junit5.DropwizardExtensionsSupport;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.kainos.ea.DropwizardWebServiceApplication;
+import org.kainos.ea.DropwizardWebServiceConfiguration;
+
+import javax.ws.rs.core.Response;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+@ExtendWith(DropwizardExtensionsSupport.class)
+public class BandIntegrationTest {
+    static final DropwizardAppExtension<DropwizardWebServiceConfiguration> APP = new DropwizardAppExtension<>(
+            DropwizardWebServiceApplication.class, null,
+            new ResourceConfigurationSourceProvider()
+    );
+    @Test
+    void getBand_shouldReturn200(){
+        Response response = APP.client().target("http://localhost:8080/api/band/1")
+                .request().get(Response.class);
+
+        assertEquals(200, response.getStatus());
+        assertTrue(response.hasEntity());
+    }
+    @Test
+    void getBand_shouldReturn400_whenIdInvalid(){
+        Response response = APP.client().target("http://localhost:8080/api/band/1000")
+                .request().get(Response.class);
+
+        assertEquals(400, response.getStatus());
+        assertTrue(response.hasEntity());
+    }
+}
--- src/test/java/org/kainos/ea/integration/JobRoleIntegrationTest.java
+++ src/test/java/org/kainos/ea/integration/JobRoleIntegrationTest.java
@@ -8,8 +8,15 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.kainos.ea.DropwizardWebServiceApplication;
 import org.kainos.ea.DropwizardWebServiceConfiguration;
+import org.kainos.ea.cli.JobRoleRequest;
 
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @ExtendWith(DropwizardExtensionsSupport.class)
 public class JobRoleIntegrationTest {
@@ -27,5 +34,40 @@ public class JobRoleIntegrationTest {
                 .get(List.class);
 
         Assertions.assertTrue(responseBody.size() > 0);
+    }
+
+    @Test
+    void createJobRole_shouldReturnIdOfCreatedJobRole(){
+        JobRoleRequest jobRoleRequest = new JobRoleRequest(
+                "Name",
+                "Spec",
+                "Responsibilities",
+                "Sharepoint",
+                1,
+                1
+        );
+
+        Response response = APP.client().target("http://localhost:8080/api/job-roles")
+                .request().post(Entity.entity(jobRoleRequest, MediaType.APPLICATION_JSON_TYPE));
+
+        assertEquals(201, response.getStatus());
+        assertTrue(response.readEntity(Integer.class) > 1);
+    }
+
+    @Test
+    void createJobRole_shouldReturn400_whenInvalidJobRole(){
+        JobRoleRequest jobRoleRequest = new JobRoleRequest(
+                "Name",
+                "Spec",
+                "Responsibilities",
+                "Sharepoint",
+                -1,
+                -1
+        );
+
+        Response response = APP.client().target("http://localhost:8080/api/job-roles")
+                .request().post(Entity.entity(jobRoleRequest, MediaType.APPLICATION_JSON_TYPE));
+
+        assertEquals(400, response.getStatus());
     }
 }
